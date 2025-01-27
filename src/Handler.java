8de9b16361f1d8a0387e4fd7dff300f992b13649
shirosora1 <13071798+shirosora1@users.noreply.github.com>
2025-01-27 21:39:16 +0000

    Added checks to see if an update has been made before showing the updated list.

--- src/Handler.java
+++ src/Handler.java
@@ -4,15 +4,21 @@ public class Handler {
 
     // A method to loop through the array list and prints out each item
 
-    public static void loopToDo(ArrayList<String> toDo) {
+    public static void loopToDo(ArrayList<String> tasks) {
+        // Checks to make sure something is in the ArrayList, if not prints
+        // a message and exits the loop
+        if (tasks.isEmpty()) {
+            System.out.println("Your to-do list is empty!");
+            return;
+        }
         // adds an incrementing number beside the to-do item
-        int number = 0;
+        int number = 1;
 
         // Enhanced for loop: 's' represents the data itself (e.g., "Cook dinner")
         // from the list, rather than an index
-        for (String s : toDo) {
-            number++;
+        for (String s : tasks) {
             System.out.println(number + ". " + s);
+            number++;
         }
     }
 }
--- src/ToDoList.java
+++ src/ToDoList.java
@@ -3,19 +3,35 @@ import java.util.ArrayList;
 public class ToDoList {
     public static void main(String[] args) {
         ArrayList<String> toDo = new ArrayList<>();
+        boolean updated = false;
 
         toDo.add("Cook dinner");
         toDo.add("Hang laundry to dry");
         toDo.add("Clean kitchen");
 
+        System.out.println("---- ToDo ----");
+
         Handler.loopToDo(toDo);
 
         toDo.set(0, "Make a brew");
+        updated = true;
+
+        if (toDo.size() > 2) {
+            String removed = toDo.remove(2);
+            updated = true;
+
+            System.out.println();
+            System.out.println("Task removed: " + removed);
+        }
 
         // separates the before and after to-do list
-        System.out.println();
 
-        Handler.loopToDo(toDo);
+        if (updated) {
+            System.out.println();
+            System.out.println("---- Updated ToDo ----");
+
+            Handler.loopToDo(toDo);
+        }
 
     }
 }
