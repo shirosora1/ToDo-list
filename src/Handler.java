d2a621240601f5e8739806d72ca4c5af70ec40be
shirosora1 <13071798+shirosora1@users.noreply.github.com>
2025-02-10 22:08:43 +0000

    Updated to allow user input, added methods for task input, updating tasks and removing them.

--- src/Handler.java
+++ src/Handler.java
@@ -1,24 +1,101 @@
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class Handler {
+   private static Scanner scanner = new Scanner(System.in);
 
-    // A method to loop through the array list and prints out each item
+    public static int menu(Scanner scanner) {
 
-    public static void loopToDo(ArrayList<String> tasks) {
-        // Checks to make sure something is in the ArrayList, if not prints
-        // a message and exits the loop
-        if (tasks.isEmpty()) {
-            System.out.println("Your to-do list is empty!");
-            return;
+        System.out.println("""
+                What would you like to do?
+                [1] Add task
+                [2] Update task
+                [3] Remove task
+                [4] Exit?""");
+
+        System.out.print("Please enter your choice: ");
+        int menuChoice = scanner.nextInt();
+        scanner.nextLine();
+
+        return menuChoice;
+    }
+
+    public static HashMap<Integer, String> taskInput(Scanner scanner) {
+
+        HashMap<Integer, String> tasks = new HashMap<>();
+
+        if (menu(scanner) == 1) {
+            System.out.println("Enter a task: ");
+            String task = scanner.nextLine();
+            int position = 1;
+
+            tasks.put(position, task);
+
+            // loops the user input so asks for a new task until the user ends it with "!!"
+            while (!task.equals("!!")) {
+
+                System.out.println("Enter a task: ");
+                task = scanner.nextLine();
+                position++;
+
+                // exits the loop once "!!" is entered
+                if (task.equals("!!")) {
+                    break;
+                }
+                // Adds tasks here so "!!" isn't included in the list
+                tasks.put(position, task);
+            }
         }
-        // adds an incrementing number beside the to-do item
-        int number = 1;
+        return tasks;
+    }
 
-        // Enhanced for loop: 's' represents the data itself (e.g., "Cook dinner")
-        // from the list, rather than an index
-        for (String s : tasks) {
-            System.out.println(number + ". " + s);
-            number++;
+    // updates the to-do list from hashmap key
+    public static void updateTodo(HashMap<Integer, String> updateToDo) {
+
+        if (menu(scanner) == 2) {
+
+            printTodo(updateToDo);
+
+            System.out.println("What number would you like to update?");
+            int taskNumber = scanner.nextInt();
+            scanner.nextLine();
+
+            System.out.println("What do you want to replace it with?: ");
+            String replacedValue = scanner.nextLine();
+
+            boolean updated = updateToDo.containsKey(taskNumber);
+
+            if (updated) {
+                updateToDo.replace(taskNumber, replacedValue);
+            }
+        }
+    }
+
+    // removes the to-do list item from hashmap key
+    public static void removeTodo(HashMap<Integer, String> removeToDo) {
+
+        if (menu(scanner) == 3) {
+
+            printTodo(removeToDo);
+
+            System.out.println("What number would you like to remove?");
+            int taskNumber = scanner.nextInt();
+            scanner.nextLine();
+
+            boolean updated = removeToDo.containsKey(taskNumber);
+
+            if (updated) {
+                System.out.println(removeToDo.get(taskNumber) + " has been removed");
+                removeToDo.remove(taskNumber);
+                System.out.println();
+            }
+        }
+    }
+
+    // prints the to-do list with numbering
+    public static void printTodo(HashMap<Integer, String> toDo) {
+        for (Integer i : toDo.keySet()) {
+            System.out.println((i) + ". " + toDo.get(i));
         }
     }
 }
--- src/ToDoList.java
+++ src/ToDoList.java
@@ -1,37 +1,19 @@
-import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
 
 public class ToDoList {
     public static void main(String[] args) {
-        ArrayList<String> toDo = new ArrayList<>();
-        boolean updated = false;
+        Scanner scanner = new Scanner(System.in);
+        HashMap<Integer, String> toDo = Handler.taskInput(scanner);
 
-        toDo.add("Cook dinner");
-        toDo.add("Hang laundry to dry");
-        toDo.add("Clean kitchen");
+        Handler.printTodo(toDo);
 
-        System.out.println("---- ToDo ----");
+        Handler.updateTodo(toDo);
+        Handler.printTodo(toDo);
 
-        Handler.loopToDo(toDo);
+        Handler.removeTodo(toDo);
+        Handler.printTodo(toDo);
 
-        toDo.set(0, "Make a brew");
-        updated = true;
-
-        if (toDo.size() > 2) {
-            String removed = toDo.remove(2);
-            updated = true;
-
-            System.out.println();
-            System.out.println("Task removed: " + removed);
-        }
-
-        // separates the before and after to-do list
-
-        if (updated) {
-            System.out.println();
-            System.out.println("---- Updated ToDo ----");
-
-            Handler.loopToDo(toDo);
-        }
-
+        scanner.close();
     }
 }
